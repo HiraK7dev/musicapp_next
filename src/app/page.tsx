409fffab3c03fcb328be87cbaedd681d3fad91bc
HiraK7dev <hirak.bhatta2004@gmail.com>
2025-04-11 16:13:11 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Hero from "./components/Hero";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Hero from "../components/Hero";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Hero />
+      <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
